4d321c095fabbd6831885a5f640530f65e695b2e
Ryan Lowe <rlowe13@me.com>
2016-10-15 18:31:37 -0300

    Original FPS

---
+++ Assets/Player/Scripts/FPSController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSController : MonoBehaviour {
+
+	public float walkSpeed = 5.0f;
+	public float jumpStrength = 10.0f;
+	public float gravityMultiplier = 1.0f;
+
+	private CharacterController _charController;
+	private float prevYSpeed;
+
+	// Use this for initialization
+	void Start () {
+		_charController = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float walkX = Input.GetAxis ("Horizontal") * walkSpeed;
+		float walkZ = Input.GetAxis ("Vertical") * walkSpeed;
+
+		Vector3 movement = new Vector3 (walkX, 0, walkZ);
+		movement = Vector3.ClampMagnitude (movement, walkSpeed);
+
+		if (_charController.isGrounded) {
+			prevYSpeed = 0.0f;
+			if (Input.GetKey (KeyCode.Space)) {
+				prevYSpeed = jumpStrength;
+			}
+		} else {
+			movement.y = prevYSpeed + Physics.gravity.y * gravityMultiplier;
+			prevYSpeed = movement.y;
+		}
+
+		movement *= Time.deltaTime;
+		// Convert the movement vector from local space to world space
+		// Move the character along the movement vector
+		_charController.Move(transform.TransformDirection (movement));
+	}
+}
---
+++ Assets/Player/Scripts/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public float XSensitivity = 5.0f;
+	public float YSensitivity = 4.0f;
+
+	private float _rotationX = 0;
+	private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+		_camera = gameObject.GetComponentInChildren<Camera> ();
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_rotationX -= Input.GetAxis ("Mouse Y") * YSensitivity;
+		_rotationX = Mathf.Clamp (_rotationX, -90f, 90f);
+
+		float delta = Input.GetAxis ("Mouse X") * XSensitivity;
+		float rotationY = transform.localEulerAngles.y + delta;
+
+		transform.localEulerAngles = new Vector3 (0, rotationY, 0);
+		_camera.transform.localEulerAngles = new Vector3 (_rotationX, 0, 0);
+	}
+}
