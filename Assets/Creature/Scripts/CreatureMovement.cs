ef724086de56a1d84ee259ca5235e90aac4721b8
ian-dawson <ian.dawson@dal.ca>
2016-10-16 12:48:37 -0300

    Update CreatureMovement.cs
    Added condition to stop movement when at player.

--- Assets/Creature/Scripts/CreatureMovement.cs
+++ Assets/Creature/Scripts/CreatureMovement.cs
@@ -14,10 +14,17 @@ public class CreatureMovement : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
-		Vector3 dir = player.transform.position - transform.position;
-		Vector3 movement = dir.normalized * 0.07f;
-		GetComponent <CharacterController>().Move (movement);
-		transform.LookAt (player.transform.position);
-	}
+	void Update () {
+        //print (Mathf.Abs((player.transform.position - transform.position).x) + Mathf.Abs((player.transform.position - transform.position).z));
+        if ((Mathf.Abs ((player.transform.position - transform.position).x) + Mathf.Abs ((player.transform.position - transform.position).z)) > 1.0f) { 
+            Vector3 dir = (player.transform.position - transform.position);
+            Vector3 movement = dir.normalized * 0.07f;
+            GetComponent <CharacterController> ().Move (movement);
+            transform.LookAt (player.transform.position);
+        } else {
+            //CONTACT ACTIONS/ANIMATIONS?
+        }
+    }
+
+
 }
